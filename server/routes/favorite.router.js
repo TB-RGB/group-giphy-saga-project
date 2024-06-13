daeeabdc28f7ae559aff41f3710da3bd2da31211
Brock Biever <brock.biever10@gmail.com>
2024-06-13 10:51:53 -0500

    basic skeleton for routes started

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,23 +5,56 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
-});
+  res.sendStatus(200)
+    const queryText = `
+      
+    `;
+    pool.query(queryText)
+      .then((result) => { res.send(result.rows); })
+      .catch((err) => {
+        console.log('Error in GET ', err);
+        res.sendStatus(500);
+      });
+  });
+;
 
 // add a new favorite
 router.post('/', (req, res) => {
+  
+  
   res.sendStatus(201);
+  pool.query(queryText, queryValues)
+  .then((result) => { res.sendStatus(201); })
+  .catch((err) => {
+    console.log('Error in POST /api/plants', err);
+    res.sendStatus(500);
+  });
 });
 
 // update a favorite's associated category
 router.put('/:id', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  pool.query(queryText, queryValues)
+    .then((result) => { res.sendStatus(200); })
+    .catch((err) => {
+      console.log('Error in PUT /:id', err);
+      res.sendStatus(500);
 });
-
+})
 // delete a favorite
 router.delete('/:id', (req, res) => {
   res.sendStatus(200);
+
+  const queryText = `
+  
+`;
+pool.query(queryText, [req.params.id])
+  .then(() => { res.sendStatus(200); })
+  .catch((err) => {
+    console.log('Error in DELETE /:id', err);
+    res.sendStatus(500);
+  });
 });
+
 
 module.exports = router;
