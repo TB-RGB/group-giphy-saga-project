396adb56eb92ccef63a729fe4c88534c01f723e0
Sam Shacter <sshacter@gmail.com>
2024-06-14 10:10:06 -0500

    working on imagelist

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,6 +1,10 @@
 import { useDispatch } from "react-redux"
 import { Button } from "@mui/material"
-
+import ImageList from '@mui/material/ImageList';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ImageListItem from '@mui/material/ImageListItem';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 const SearchItem =({ giphy })=>{
     const dispatch = useDispatch()
 
@@ -14,9 +18,31 @@ const SearchItem =({ giphy })=>{
         <div>
             <div className="search-item">
                 <div className="image-container">
-                <img src={giphy.images.original.url} alt="" className="image"/>
+                    <ImageList >
+                        <ImageListItem key={giphy.images.original.url}> 
+                        <img src={giphy.images.original.url} alt="" className="image"/>
+                        <ImageListItemBar
+            
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+
+
+                        </ImageListItem>
+                
+                    
+                
+                </ImageList>
                 </div>
+                
                 <Button onClick={()=>addFavorite()}>Add Favorite</Button>
+                
             </div>
             
         </div>
--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,11 +1,26 @@
 import { useSelector } from "react-redux"
 import SearchItem from "../SearchItem/SearchItem"
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+
+
+
+
 import './SearchList.css'
  const SearchList = () => {
 const giphyList = useSelector(store => store.searchResults)
 console.log('giphyList', giphyList)
     return (
         <>
+
+      
+          
+        
+      
+    
         <div className="search-list-container">
         {giphyList.map((giphy) => 
             <SearchItem key={giphy.id} giphy={giphy}/>
